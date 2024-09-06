352f152223e863ae267f24302d69fe7a8ed23ce4
mujtabaa11 <Mohamed.Mugtaba11@gmail.com>
2024-09-06 05:31:43 -0400

    Final commit

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 
 const input = document.getElementById("text-input");
 const checkButton = document.getElementById("check-btn");
-const word = input.value;
+const result = document.getElementById("result");
+
+function cleanInputString(str) {
+  const regex = /[\W_]/g;
+  return str.replace(regex, '');
+}
 
 checkButton.addEventListener("click", () => {
-  if (word === "") {
+  if (input.value === "") {
     alert("Please input a value");
+
   } else {
-    
-  }
-});
+    const cleanWord = cleanInputString(input.value).toLowerCase();
+    const revWord = cleanWord.split("").reverse().join("");
+
+    if (cleanWord === revWord) {
+      result.innerText = `${input.value} is a palindrome`
+      } else {
+        result.innerText = `${input.value} is not a palindrome`
+      }
+}});
